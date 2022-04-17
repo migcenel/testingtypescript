e82905a6cbd36f8e53d9eff101f9cde4fb2d5b4b
ArKnHell <migcenel@gmail.com>
2022-04-17 15:43:57 -0500

    adding new file to test numbers type

---
+++ src/numbers.ts
@@ -0,0 +1,12 @@
+(() =>{
+    let productPrice = 1154;
+    console.log ('product price: ', productPrice);
+
+    let productInStock : number;
+
+    productInStock = 10;
+
+    if (productInStock > 10){
+        console.log ("we have stock");
+    }
+})();
